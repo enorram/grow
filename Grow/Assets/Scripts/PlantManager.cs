a103962e8f16948c03114d88db4dd05d10fd4c5f
Eli Marrone <elmarron@microsoft.com>
2018-07-31 16:42:22 -0700

    In which our hero makes a grid
    - Removed the GridManager but retained GridManager
    script for reference. Best if maintained as growing
    plant object with LineRenderer and growing list of
    buds, so opted for a PlantManager instead
    
    - Created GridMarker objects, a plant start point,
    and a Bud script
    
    ToDo:
    - Take space bar input to do something
    - When space bar hit, render line between two points
    - Initiate all Grid Markers with knowledge about
    their neighbors
    - put in logic to make rendering line a generic
    process applied to all Buds in list
    - Add splitting logic
    - Add shape drawer
    - add shape checking/conditional growing
    - add enemies
    - add burning walls
    - mess around with making line render pretty
    - mess around with grid marker collision initiating
    a looping sound (calm and not annoying)
    - mess around with making everything pretty

---
+++ Grow/Assets/Scripts/PlantManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantManager : MonoBehaviour {
+
+    // singleton pattern allows this to be called from other scripts
+    public static PlantManager instance; 
+
+    // create list of Buds
+    public List<Bud> bud_list = new List<Bud>();
+
+    // list of grid objects
+    public List<GridMarker> gridMarkers = new List<GridMarker>();
+
+    // Use this for initialization
+    void Start () {
+        instance = GetComponent<PlantManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void GrowAll()
+    {
+
+    }
+
+    void InitiateGridMarkers()
+    {
+
+    }
+}
